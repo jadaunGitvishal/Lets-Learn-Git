5f4534faeab434e31ffee3f4844de509e97d2b2c
Vishal Jadaun <vishaljadaun@metamix.tech>
2023-06-28 17:49:57 +0530

    added Multiply

--- Sum.java
+++ Sum.java
@@ -4,6 +4,6 @@ public static void main(String args[])
 {  
 int n1 = 225, n2 = 115, sum;  
 sum = n1 + n2;  
-System.out.println("The sum of numbers is: "+sum);  
+System.out.println("The sum of my stylr in the: "+sum);  
 }  
 }  
---
+++ multiply.java
@@ -0,0 +1,11 @@
+public class Multiply {
+
+
+    public static void main (String [] args) {
+        int a = 4;
+        INT B = 2;
+
+
+        System.out.println("the product is " + (a*B))
+    }
+}
